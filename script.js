8855212575a11cdf9993a738dca22ed0cc3163d6
todoexp <84766701+todoexp@users.noreply.github.com>
2021-05-25 03:30:02 -0400

    Update script.js
    Tried to fix the weather icon image problem

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ window.onload = function () {
                     const { description, icon } = data.weather[0];
                     const { sunrise, sunset } = data.sys;
 
-                    const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+                    const iconUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`;
                     const fahrenheit = (temp * 9) / 5 + 32;
 
                     const sunriseGMT = new Date(sunrise * 1000);
